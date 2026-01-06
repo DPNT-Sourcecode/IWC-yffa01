294f8a8c1409564bbfaeda49e387703edd74ba1d
sbx_user1051 <sbx_user1051@169.254.82.155>
2026-01-06 22:43:13 +0000

    Tue Jan 06 22:43:13 UTC 2026

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -46,3 +46,17 @@ def test_enqueue_bank_statements_dequeue_flow_with_age_2() -> None:
         call_dequeue().expect("id_verification", 2),
         call_dequeue().expect("bank_statements", 2),
     ])
+
+def test_enqueue_bank_statements_dequeue_flow_with_age_3() -> None:
+    run_queue([
+        call_enqueue("bank_statements", 1, iso_ts(delta_minutes=0)).expect(1),
+        call_enqueue("companies_house", 2, iso_ts(delta_minutes=0)).expect(2),
+        call_enqueue("id_verification", 3, iso_ts(delta_minutes=6)).expect(3),
+        call_enqueue("bank_statements", 3, iso_ts(delta_minutes=7)).expect(4),
+        call_size().expect(4),
+        call_dequeue().expect("bank_statements", 1),
+        call_dequeue().expect("companies_house", 2),
+        call_dequeue().expect("id_verification", 2),
+        call_dequeue().expect("bank_statements", 2),
+    ])
+
